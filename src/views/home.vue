bf2e3b8af41bfd963ac2b2c4bb1dd0d2451ca20d
779102260 <779102260@qq.com>
2019-04-16 22:30:06 +0800

    文件名 大小更正

---
+++ src/views/home.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <ImgSilde/>
+    </div>
+</template>   
+<style>
+.container{
+    width: 80%;
+    margin:0 auto;
+}
+</style>
+
+<script>
+import ImgSilde from "@/components/imgslide.vue"
+
+
+export default {
+    name:"home",
+    data(){
+        return {
+
+        }
+    },
+    components:{
+        ImgSilde
+    }
+}
+</script>
